3e9e794da8e4da1c196a6f744cef52c8496791b6
Andres Duarte <duartebotero@gmail.com>
2021-09-29 17:24:21 -0400

    Lesson 49 - add roles to users

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,7 +4,7 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable, :trackable,:confirmable
 
-  has_many :courses
+  rolify
 
   def to_s
     email
@@ -12,6 +12,21 @@ class User < ApplicationRecord
 
   def username
     self.email.split(/@/).first
+  end
+
+  has_many :courses
+
+  after_create :assign_default_role
+
+  def assign_default_role
+    if User.count == 1
+      self.add_role(:admin) if self.roles.blank?
+      self.add_role(:teacher)
+      self.add_role(:student)
+    else
+      self.add_role(:student) if self.roles.blank?
+      self.add_role(:teacher) #if you want any user to be able to create own courses
+    end
   end
 
 end
