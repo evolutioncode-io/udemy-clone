9a92123e10fb5783f913fbe2df0beee705e9bd27
Andres Duarte <duartebotero@gmail.com>
2021-09-28 23:49:36 -0400

    lesson 41

--- db/seeds.rb
+++ db/seeds.rb
@@ -3,7 +3,11 @@ User.create!(email: 'admin@example.com', password: 'admin@example.com', password
 30.times do
   Course.create!([{
     title: Faker::Educator.course_name,
-    description: Faker::TvShows::GameOfThrones.quotes,
-    user_id: User.first.id
-    }])
+    description: Faker::TvShows::GameOfThrones.quote,
+    user_id: User.first.id,
+    short_description: Faker::Quote.famous_last_words,
+    language: Faker::ProgrammingLanguage.name,
+    level: 'Beginner',
+    price: Faker::Number.between(from: 1000, to: 20000)
+  }])
 end
