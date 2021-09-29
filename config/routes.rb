c91ef8c973eeaa0c803c29fb6a045dec92d5b80b
Andres Duarte <duartebotero@gmail.com>
2021-09-29 16:37:49 -0400

    lesson 46

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
   devise_for :users
-  resources :users, only: [:index]
-  root "home#index"
   resources :courses
+  resources :users, only: [:index]
+  get 'home/index'
+  get 'home/activity'
+  root 'home#index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
