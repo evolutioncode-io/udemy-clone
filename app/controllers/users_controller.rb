2b83920924b2684c4d21290b13eddb29f5874757
Andres Duarte <duartebotero@gmail.com>
2021-09-29 11:29:37 -0400

    ramsack

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,8 @@
 class UsersController < ApplicationController
   def index
-    @users = User.all
+    # @users = User.all.order(create_at: :desc)
+
+    @q = User.ransack(params[:q])
+    @users = @q.result(distinct: true)
   end 
 end
