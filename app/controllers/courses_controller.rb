e7d8197e4bf638f2e52efe98bc2c72c9805f09a7
Andres Duarte <duartebotero@gmail.com>
2021-10-02 19:25:55 -0400

    lesson 80

--- app/controllers/courses_controller.rb
+++ app/controllers/courses_controller.rb
@@ -1,7 +1,6 @@
 class CoursesController < ApplicationController
-  before_action :set_course, only: %i[ show edit update destroy ]
+  before_action :set_course, only: [:show, :edit, :update, :destroy]
 
-  # GET /courses or /courses.json
   def index
     #if params[:title]
     #  @courses = Course.where('title ILIKE ?', "%#{params[:title]}%") #case-insensitive
@@ -11,20 +10,33 @@ class CoursesController < ApplicationController
     #  #@q = Course.ransack(params[:q])
     #  #@courses = @q.result.includes(:user)
     #end
+
+    #if current_user.has_role?(:admin)
+    #  @ransack_courses = Course.ransack(params[:courses_search], search_key: :courses_search)
+    #  @courses = @ransack_courses.result.includes(:user)
+    #else
+    #  redirect_to root_path, alert: 'You do not have access'
+    #end
+
+    @ransack_path = courses_path
+
     @ransack_courses = Course.ransack(params[:courses_search], search_key: :courses_search)
-    # @courses = @ransack_courses.result.includes(:user)
+    #@courses = @ransack_courses.result.includes(:user)
     @pagy, @courses = pagy(@ransack_courses.result.includes(:user))
 
   end
 
   def purchased
-    # @courses = Course.joins(:enrollments).where(enrollments: {user: current_user})
-    @pagy, @courses = pagy(Course.joins(:enrollments).where(enrollments: {user: current_user}))
+    @ransack_path = purchased_courses_path
+    @ransack_courses = Course.joins(:enrollments).where(enrollments: {user: current_user}).ransack(params[:courses_search], search_key: :courses_search)
+    @pagy, @courses = pagy(@ransack_courses.result.includes(:user))
     render 'index'
   end
 
   def pending_review
-    @pagy, @courses = pagy(Course.joins(:enrollments).merge(Enrollment.pending_review.where(user: current_user)))
+    @ransack_path = pending_review_courses_path
+    @ransack_courses = Course.joins(:enrollments).merge(Enrollment.pending_review.where(user: current_user)).ransack(params[:courses_search], search_key: :courses_search)
+    @pagy, @courses = pagy(@ransack_courses.result.includes(:user))
     render 'index'
   end
 
@@ -39,18 +51,15 @@ class CoursesController < ApplicationController
     @lessons = @course.lessons
   end
 
-  # GET /courses/new
   def new
     @course = Course.new
     authorize @course
   end
 
-  # GET /courses/1/edit
   def edit
     authorize @course
   end
 
-  # POST /courses or /courses.json
   def create
     @course = Course.new(course_params)
     authorize @course
@@ -58,46 +67,42 @@ class CoursesController < ApplicationController
 
     respond_to do |format|
       if @course.save
-        format.html { redirect_to @course, notice: "Course was successfully created." }
+        format.html { redirect_to @course, notice: 'Course was successfully created.' }
         format.json { render :show, status: :created, location: @course }
       else
-        format.html { render :new, status: :unprocessable_entity }
+        format.html { render :new }
         format.json { render json: @course.errors, status: :unprocessable_entity }
       end
     end
   end
 
-  # PATCH/PUT /courses/1 or /courses/1.json
   def update
     authorize @course
     respond_to do |format|
       if @course.update(course_params)
-        format.html { redirect_to @course, notice: "Course was successfully updated." }
+        format.html { redirect_to @course, notice: 'Course was successfully updated.' }
         format.json { render :show, status: :ok, location: @course }
       else
-        format.html { render :edit, status: :unprocessable_entity }
+        format.html { render :edit }
         format.json { render json: @course.errors, status: :unprocessable_entity }
       end
     end
   end
 
-  # DELETE /courses/1 or /courses/1.json
   def destroy
     authorize @course
     @course.destroy
     respond_to do |format|
-      format.html { redirect_to courses_url, notice: "Course was successfully destroyed." }
+      format.html { redirect_to courses_url, notice: 'Course was successfully destroyed.' }
       format.json { head :no_content }
     end
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
     def set_course
       @course = Course.friendly.find(params[:id])
     end
 
-    # Only allow a list of trusted parameters through.
     def course_params
       params.require(:course).permit(:title, :description, :short_description, :price, :language, :level)
     end
