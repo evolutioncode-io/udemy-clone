6bdd7e8fc8f322b58e67d7614e377897c3d95afb
Andres Duarte <duartebotero@gmail.com>
2021-10-06 15:29:55 -0400

    129

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,16 @@
 Rails.application.routes.draw do
+
+  root 'home#index'
+  get 'home/index'
+  get 'activity', to: 'home#activity'
+  get 'analytics', to: 'home#analytics'
+
   resources :enrollments do
     get :my_students, on: :collection
   end
+
   devise_for :users
+
   resources :courses do
     get :pending_review, on: :collection
     get :purchased, on: :collection
@@ -12,7 +20,8 @@ Rails.application.routes.draw do
       patch :approve
       patch :unapprove
     end
-    resources :lessons do
+    resources :lessons, except: [:index] do
+      resources :comments, except: [:index]
       put :sort
       member do
         delete :delete_video
@@ -24,17 +33,11 @@ Rails.application.routes.draw do
   resources :youtube, only: :show
   
   resources :users, only: [:index, :edit, :show, :update]
-  root 'home#index'
-  get 'home/index'
-  get 'activity', to: 'home#activity'
-  get 'analytics', to: 'home#analytics'
-
+  
   namespace :charts do
     get 'users_per_day'
     get 'enrollments_per_day'
     get 'course_popularity'
     get 'money_makers'
   end
-  
-  
 end
