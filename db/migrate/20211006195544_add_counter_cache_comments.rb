908c05859fee1fece75ad52e3fc9ba4f94182805
Andres Duarte <duartebotero@gmail.com>
2021-10-06 20:24:18 -0400

    133

--- db/migrate/20211006195544_add_counter_cache_comments.rb
+++ db/migrate/20211006195544_add_counter_cache_comments.rb
@@ -1,6 +1,6 @@
 class AddCounterCacheComments < ActiveRecord::Migration[6.1]
   def change
-    add_column :users, :comments_count, :integer, null: false, default: 0
-    add_column :lessons, :comments_count, :integer, null: false, default: 0
+    add_column :lessons, :user_lessons_count, :integer, null: false, default: 0
+    add_column :users, :user_lessons_count, :integer, null: false, default: 0
   end
 end
